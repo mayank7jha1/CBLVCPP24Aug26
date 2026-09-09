d9f38cacaaa58379138bb9b39fb9842cc4d490b2
Mayank Jha <hocrux92@gmail.com>
2026-09-09 22:02:31 +0530

    Lecture 02

---
+++ Lecture02/InputAndOutput.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // int x;
+  // int y;
+  // int z;
+  // int t;
+
+  // cin >> x;
+  // cin >> y;
+  // cin >> z;
+  // cin >> t;
+
+  // cout << x;
+  // cout << " ";
+  // cout << y;
+  // cout << " ";
+  // cout << z;
+  // cout << " ";
+  // cout << t;
+
+  int x, y, z, t;
+  cin >> x >> y >> z >> t;
+  cout << x << "\n" << y << endl << z << " " << t;
+}
